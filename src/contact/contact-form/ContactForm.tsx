4418a24901384c67045f00b984aff4b7ce5f695e
vlad1907 <58011613+vlad1907@users.noreply.github.com>
2023-02-27 00:53:28 +0200

    feat: form is alive and working :)

--- src/contact/contact-form/ContactForm.tsx
+++ src/contact/contact-form/ContactForm.tsx
@@ -2,16 +2,28 @@ import React from 'react';
 import s from './ContactForm.module.scss';
 import d from '../../common/components/button/Button.module.scss';
 import {useFormik} from "formik";
-
+import * as Yup from 'yup';
+import emailjs from '@emailjs/browser';
 const ContactForm = () => {
+
     const formik = useFormik({
         initialValues: {
             name: '',
             email: '',
             message: ''
         },
-        onSubmit: values => {
-            alert(`${values.name} ${values.message}`)
+        validationSchema: Yup.object({
+            name: Yup.string().max(50, 'Max length 50 symbols').required('name required'),
+            email: Yup.string().required('Required').email('Invalid email format'),
+            message: Yup.string().max(1000, 'Max length 1000 symbols').required('message required')
+        }),
+        onSubmit: (values) => {
+            debugger
+            emailjs.send('service_ppo35ui', 'template_tr0c7as', values, 'wpCy17UR47MgWBbhz')
+                .then(res => {
+                    formik.resetForm()
+                    alert('successes')
+                })
         }
     });
     return (
@@ -26,6 +38,7 @@ const ContactForm = () => {
                     value={formik.values.name}
                     placeholder={'Your name'}
                 />
+                {formik.touched.name && formik.errors.name && <div style={{color: 'red'}}>{formik.errors.name}</div>}
                 <input
                     className={s.textField}
                     placeholder={'Your email'}
@@ -35,6 +48,7 @@ const ContactForm = () => {
                     onChange={formik.handleChange}
                     value={formik.values.email}
                 />
+                {formik.touched.email && formik.errors.email && <div style={{color: 'red'}}>{formik.errors.email}</div>}
                 <textarea
                     className={s.textarea}
                     placeholder={'Your message'}
@@ -43,7 +57,7 @@ const ContactForm = () => {
                     onChange={formik.handleChange}
                     value={formik.values.message}
                 />
-
+                {formik.touched.message && formik.errors.message && <div style={{color: 'red'}}>{formik.errors.message}</div>}
                 <div className={s.buttonContainer}>
                     <button type={'submit'} className={`${s.button} ${d.button}`}>Send</button>
                 </div>
